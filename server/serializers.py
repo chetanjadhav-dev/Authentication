e806a2e00cfbc9030581a4bd5d2d65f682c4910c
Chetan <chetanjadhav.dev@gmail.com>
2024-06-21 10:31:16 +0530

    user profile

--- server/serializers.py
+++ server/serializers.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email',  'password', 'is_staff', 'is_active',
+                  'last_login', 'date_joined']
